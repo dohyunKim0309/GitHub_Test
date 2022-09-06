1e0259e57279a014d92d56e2eef8231b4c7c1a18
kimde30 <kimde30@yonsei.ac.kr>
2022-09-06 21:11:45 +0900

    change

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ class project:
 
     def sayhello(self):
         print("hello, my name is {}!".format(self.name))
-
+daklfja;dlkfjdlkf
     def presentTime(self):
         print("Time is {}".format(time.ctime()))
 
